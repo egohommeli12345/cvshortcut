7742935938e90b85d11a6bb51db15abf27dff00c
Samuli Pirnes <pirnes.samuli@gmail.com>
2026-01-13 12:22:30 +0200

    Add discount field

--- src/components/PaymentElement.tsx
+++ src/components/PaymentElement.tsx
@@ -1,9 +1,10 @@
+import checkoutStyles from "./Checkout.module.css";
 import styles from "./PaymentElement.module.css";
-import {Elements} from "@stripe/react-stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import Checkout from "@/components/Checkout";
-import {useCallback, useEffect, useState} from "react";
-import {useResumeContext} from "@/components/ResumeContext";
-import {loadStripe} from "@stripe/stripe-js";
+import { useCallback, useEffect, useState } from "react";
+import { useResumeContext } from "@/components/ResumeContext";
+import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC!);
 
@@ -14,8 +15,11 @@ const PaymentElement = () => {
     updateField,
     resetResumeData,
     saveResumeData,
-    loadResumeData
+    loadResumeData,
   } = useResumeContext();
+
+  const [coupon, setCoupon] = useState<string>("");
+  const [pdf, setPdf] = useState<string>("");
 
   // For Stripe
   const [clientSecret, setClientSecret] = useState<string>("");
@@ -37,6 +41,23 @@ const PaymentElement = () => {
     clientSecret: clientSecret,
   };
 
+  const checkDiscount = async (code: string) => {
+    const response = await fetch("/api/check_discount", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ discountCode: code, data: resumeData }),
+    });
+    if (response.ok) {
+      const blob = await response.blob();
+
+      const url = window.URL.createObjectURL(blob);
+      setPdf(url);
+      window.open(url, "_blank");
+    }
+  };
+
   useEffect(() => {
     const clientSecret = new URLSearchParams(window.location.search).get(
       "payment_intent_client_secret"
@@ -48,18 +69,35 @@ const PaymentElement = () => {
   }, []);
   return (
     <>
-      {!clientSecret &&
-        <button className={styles.greenbtn} onClick={initPayment}>
-          Download PDF for 2.29 €
-        </button>}
+      {!clientSecret && (
+        <>
+          <button className={styles.greenbtn} onClick={initPayment}>
+            Download PDF for 2.29 €
+          </button>
+          <div className={checkoutStyles.discount}>
+            <input
+              type="text"
+              className={checkoutStyles.discountinput}
+              value={coupon}
+              onChange={(e) => setCoupon(e.target.value)}
+              placeholder={"Free resume coupon"}
+            />
+            <button
+              type={"button"}
+              className={checkoutStyles.discountbtn}
+              onClick={() => checkDiscount(coupon)}
+            >
+              Check coupon
+            </button>
+          </div>
+        </>
+      )}
 
-      {stripePromise && clientSecret &&
-        <Elements
-          stripe={stripePromise}
-          options={options}
-        >
-          <Checkout clientSecret={clientSecret}/>
-        </Elements>}
+      {stripePromise && clientSecret && (
+        <Elements stripe={stripePromise} options={options}>
+          <Checkout clientSecret={clientSecret} />
+        </Elements>
+      )}
     </>
   );
 };
